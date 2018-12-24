57f846e5aca439cdf3c8d381176e8f96fadb0524
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-24 17:41:59 -0600

    added delete user and delete route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -272,4 +272,19 @@ router.delete(
     }
   )
 
+  // @router DELETE api/profile
+// @desc Delete user and profile
+// @access Private
+router.delete(
+    '/',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Profile.findOneAndRemove({user:req.user.id})
+        .then(()=>{
+           User.findOneAndRemove({_id:req.user.id})
+           .then(()=>res.json({success: true}))
+        })
+    }
+  )
+
 module.exports = router

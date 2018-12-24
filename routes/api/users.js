9b88ca24e136f2c346b414f99c4696fb39d441ee
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-24 14:40:20 -0600

    added profile route

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const key = require('../../config/keys')
+const keys = require('../../config/keys')
 const passport = require('passport')
 
 // load Input Validation
@@ -23,43 +23,43 @@ router.get('/test', (req, res) => res.json({ msg: 'users works' }))
 // @desc Register user
 // @access Public
 router.post('/register', (req, res) => {
-  const { errors, isValid } = validateRegisterInput(req.body);
+  const { errors, isValid } = validateRegisterInput(req.body)
 
   // Check Validation
   if (!isValid) {
-    return res.status(400).json(errors);
+    return res.status(400).json(errors)
   }
 
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      errors.email = 'Email already exists';
-      return res.status(400).json(errors);
+      errors.email = 'Email already exists'
+      return res.status(400).json(errors)
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200', // Size
         r: 'pg', // Rating
         d: 'mm' // Default
-      });
+      })
 
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
         avatar,
         password: req.body.password
-      });
+      })
 
       bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser.password = hash;
+          if (err) throw err
+          newUser.password = hash
           newUser
             .save()
             .then(user => res.json(user))
-            .catch(err => console.log(err));
-        });
-      });
+            .catch(err => console.log(err))
+        })
+      })
     }
-  });
+  })
 })
 
 // @router GET api/users/login
@@ -96,7 +96,7 @@ router.post('/login', (req, res) => {
           (err, token) => {
             res.json({
               success: true,
-              token: 'Bearer' + token
+              token: 'Bearer ' + token
             })
           }
         )

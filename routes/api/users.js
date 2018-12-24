dc3dca0c7ac7feb573a3523a1db55349269cdd84
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-23 21:28:33 -0600

    added login validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,6 +9,7 @@ const passport = require('passport')
 
 // load Input Validation
 const validateRegisterInput = require('../../validation/register')
+const validateLoginInput = require('../../validation/login')
 
 // Load User modal
 const User = require('../../models/User')
@@ -65,6 +66,12 @@ router.post('/register', (req, res) => {
 // @desc Login user/ returning jwt token
 // @access Public
 roueter.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body)
+
+  // check validation
+  if (!isValid) {
+    return res.status(400).json(errors)
+  }
   const email = req.body.email
   const password = req.body.password
 
@@ -72,7 +79,8 @@ roueter.post('/login', (req, res) => {
   User.findOne({ email }).then(user => {
     // check for user
     if (!user) {
-      return res.status(404).json({ email: 'User not found' })
+      errors.email = 'User not found'
+      return res.status(404).json(errors)
     }
     // Check Password
 
@@ -93,7 +101,8 @@ roueter.post('/login', (req, res) => {
           }
         )
       } else {
-        return res.status(400).json({ password: 'Password Incorrect' })
+        errors.password = 'Password Incorrect'
+        return res.status(400).json(errors)
       }
     })
   })
--- validation/register.js
+++ validation/register.js
@@ -4,9 +4,9 @@ const Validator = require('validator')
 module.exports = function validateRegisterInput (data) {
   let errors = {}
   data.name = !isEmpty(data.name) ? data.name : ''
-  data.name = !isEmpty(data.email) ? data.email : ''
-  data.name = !isEmpty(data.password) ? data.password : ''
-  data.name = !isEmpty(data.password2) ? data.password2 : ''
+  data.email = !isEmpty(data.email) ? data.email : ''
+  data.password = !isEmpty(data.password) ? data.password : ''
+  data.password2 = !isEmpty(data.password2) ? data.password2 : ''
 
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = 'Name must be between 2 and 30 characters'
@@ -27,7 +27,7 @@ module.exports = function validateRegisterInput (data) {
   if (Validator.isEmpty(data.password2)) {
     errors.password2 = 'Confirm password filed is required'
   }
-  if (Validator.equals(data.password, data.password2)) {
+  if (!Validator.equals(data.password, data.password2)) {
     errors.password2 = 'Passwords must match'
   }
 

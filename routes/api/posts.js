990d1f120f01d4f7d628e8e8627929679c92616e
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-25 14:00:37 -0600

    added get posts route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -15,9 +15,19 @@ const validatePostInput = require('../../validation/post')
 // @access Public
 router.get('/test', (req, res) => res.json({ msg: 'post works' }))
 
+// @router GET api/posts
+// @descG get posts
+// @access Public
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404))
+})
+
 // @router POST api/posts
 // @desc Create post
-// @access Privare
+// @access Private
 
 router.post(
   '/',

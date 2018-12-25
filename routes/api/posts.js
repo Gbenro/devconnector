ebad664c6fd51e2b98ca592d2181ee1e5cc27b79
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-25 15:01:21 -0600

    added delete comment route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -157,6 +157,9 @@ router.post(
   }
 )
 
+// @router POST api/posts/comment/:id
+// @desc Add comment to a post
+// @access Private
 router.post(
   '/comment/:id',
   passport.authenticate('jwt', { session: false }),
@@ -187,8 +190,39 @@ router.post(
   }
 )
 
-// @router Delete api/posts/comments/:id
-// @desc add a comment to  a post
+// @router DELETE api/posts/comment/:id/:comment_id
+// @desc Remove comment from post
 // @access Private
+router.delete(
+  '/comment/:id/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body)
+
+    Post.findById(req.params.id)
+      .then(post => {
+        // Check to see if the comment exist
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ commentnotexists: 'Comment does not exist' })
+        }
+        // Get remove index
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id)
+
+        // Splice comment out of array
+        post.comments.splice(removeIndex, 1)
+
+        post.save().then(post => res.json(post))
+      })
+      .catch(err => res.status(404).json({ postnotfound: 'No post fpund' }))
+  }
+)
 
 module.exports = router

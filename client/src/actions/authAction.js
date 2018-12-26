2fcbfe3807377fce8d754f40b81c0b21b1995cfc
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-25 18:58:19 -0600

    set Auth token to header

--- client/src/actions/authAction.js
+++ client/src/actions/authAction.js
@@ -13,3 +13,24 @@ export const registerUser = (userData, history) => dispatch => {
       })
     )
 }
+
+// Login - Get User Token
+
+export const loginUser = userData => dispatch => {
+  axios
+    .post('api/users/login', userData)
+    .then(res => {
+      // save to local storage
+      const { token } = res.data
+      // set token to local storage
+      localStorage.setItem('jwtToken', token)
+      // Set token to Auth header
+      setAuthToken(token)
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    )
+}

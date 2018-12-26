dfbf7877af04cccdc6bdf4f109a72d70423ce3bf
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-26 17:09:47 -0600

    added social media input for create profile

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -36,7 +36,52 @@ class CreateProfile extends Component {
     const { errors, displaySocialInputs } = this.state
     let socialInputs
     if (displaySocialInputs) {
-      socialInputs = <div />
+      socialInputs = (
+        <div>
+          <InputGroup
+            placeholder='Twitter Profile URL'
+            name=' twitter'
+            icon='fab fa-twitter'
+            onChange={this.onChange}
+            error={errors.twitter}
+          />
+          <InputGroup
+            placeholder='Facebook Page URL'
+            name='facebook'
+            icon='fab fa-facebook'
+            value={this.state.facebook}
+            onChange={this.onChange}
+            error={errors.facebook}
+          />
+
+          <InputGroup
+            placeholder='Linkedin Profile URL'
+            name='linkedin'
+            icon='fab fa-linkedin'
+            value={this.state.linkedin}
+            onChange={this.onChange}
+            error={errors.linkedin}
+          />
+
+          <InputGroup
+            placeholder='YouTube Channel URL'
+            name='youtube'
+            icon='fab fa-youtube'
+            value={this.state.youtube}
+            onChange={this.onChange}
+            error={errors.youtube}
+          />
+
+          <InputGroup
+            placeholder='Instagram Page URL'
+            name='instagram'
+            icon='fab fa-instagram'
+            value={this.state.instagram}
+            onChange={this.onChange}
+            error={errors.instagram}
+          />
+        </div>
+      )
     }
 
     // Select options for status

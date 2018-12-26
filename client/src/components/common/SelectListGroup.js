c334af2dbf6757c06e18537a6810f9c30f6aa197
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-26 16:51:29 -0600

    added select list for create profile input

--- client/src/components/common/SelectListGroup.js
+++ client/src/components/common/SelectListGroup.js
@@ -2,18 +2,10 @@ import React from 'react'
 import classnames from 'classnames'
 import PropTypes from 'prop-types'
 
-const SelectListGroup = ({
-  name,
-
-  value,
-  error,
-  info,
-  onChange,
-  options
-}) => {
+const SelectListGroup = ({ name, value, error, info, onChange, options }) => {
   const selectOptions = options.map(option => (
-    <option key={option.label} value={option.vale}>
-      {option}
+    <option key={option.label} value={option.value}>
+      {option.label}
     </option>
   ))
   return (
@@ -28,20 +20,18 @@ const SelectListGroup = ({
       >
         {selectOptions}
       </select>
-
-      {info && <small className='form-text text-muted'> {info}</small>}
-      {error && <div className='invalid-feedback'>{error} </div>}
+      {info && <small className='form-text text-muted'>{info}</small>}
+      {error && <div className='invalid-feedback'>{error}</div>}
     </div>
   )
 }
 
 SelectListGroup.propTypes = {
-  name: PropTypes.string,
+  name: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   info: PropTypes.string,
   error: PropTypes.string,
-
-  onChange: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
   options: PropTypes.array.isRequired
 }
 

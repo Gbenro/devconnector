47f77fec53ed02afa214b5e7480f7da693f498b2
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-26 15:52:22 -0600

    added private Route

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import { getCurrentProfile } from '../../actions/profileActions'
 import Spinner from '../common/Spinner'
 
@@ -17,7 +18,21 @@ class Dashboard extends Component {
     if (profile == null || loading) {
       dashboardContent = <Spinner />
     } else {
-      dashboardContent = <h1>Hello</h1>
+      // check if logged in user has profile data
+      if (Object.keys(profile).length > 0) {
+        dashboardContent = <h4>TODO: Display Profile</h4>
+      } else {
+        // User is logged in but has no profile
+        dashboardContent = (
+          <div>
+            <p className='lead text-muted'>Welcome {user.name}</p>
+            <p>You have not set up a profile, please add some info</p>
+            <Link to='/create-profile' className='btn btn-lg btn-info '>
+              Create Profile
+            </Link>
+          </div>
+        )
+      }
     }
     return (
       <div className='dashboard'>

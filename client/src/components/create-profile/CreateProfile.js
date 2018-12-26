4553683b7462dd1326f4240abb8d1e62e9af570d
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-26 17:08:49 -0600

    added all input for create profile

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -33,7 +33,12 @@ class CreateProfile extends Component {
     e.preventDefault()
   }
   render () {
-    const { errors } = this.state
+    const { errors, displaySocialInputs } = this.state
+    let socialInputs
+    if (displaySocialInputs) {
+      socialInputs = <div />
+    }
+
     // Select options for status
     const options = [
       { label: '* Select Professional Status', value: 0 },
@@ -74,6 +79,75 @@ class CreateProfile extends Component {
                   onChange={this.onChange}
                   options={options}
                   info='Give us an idea of where you are at in your career'
+                />
+                <TextFieldGroup
+                  placeholder='Company'
+                  name='company'
+                  value={this.state.company}
+                  error={errors.company}
+                  onChange={this.onChange}
+                  info='Could be your own company or one that you work for'
+                />
+                <TextFieldGroup
+                  placeholder='Website'
+                  name='website'
+                  value={this.state.website}
+                  onChange={this.onChange}
+                  error={errors.website}
+                  info='Could be your own website or a company one'
+                />
+                <TextFieldGroup
+                  placeholder='Location'
+                  name='location'
+                  value={this.state.location}
+                  onChange={this.onChange}
+                  error={errors.location}
+                  info='City or city & state suggested (eg. Boston, MA)'
+                />
+                <TextFieldGroup
+                  placeholder='* Skills'
+                  name='skills'
+                  value={this.state.skills}
+                  onChange={this.onChange}
+                  error={errors.skills}
+                  info='Please use comma separated values (eg.
+                    HTML,CSS,JavaScript,PHP'
+                />
+                <TextFieldGroup
+                  placeholder='Github Username'
+                  name='githubusername'
+                  value={this.state.githubusername}
+                  onChange={this.onChange}
+                  error={errors.githubusername}
+                  info='If you want your latest repos and a Github link, include your username'
+                />
+                <TextAreaFieldGroup
+                  placeholder='Short Bio'
+                  name='bio'
+                  value={this.state.bio}
+                  onChange={this.onChange}
+                  error={errors.bio}
+                  info='Tell us a little about yourself'
+                />
+
+                <div className='mb-3'>
+                  <button
+                    onClick={() => {
+                      this.setState(prevState => ({
+                        displaySocialInputs: !prevState.displaySocialInputs
+                      }))
+                    }}
+                    className='btn btn-light'
+                  >
+                    Add Social Network Links
+                  </button>
+                  <span className='text-muted'>Optional</span>
+                </div>
+                {socialInputs}
+                <input
+                  type='submit'
+                  value='Submit'
+                  className='btn btn-info btn-block mt-4'
                 />
               </form>
             </div>

801501e3bec190fa3fbf0e9c864cd85995bb6334
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-27 15:10:56 -0600

    added about component in profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import ProfileHeader from './ProfileHeader'
 import ProfileAbout from './ProfileAbout'
 import ProfileCreds from './ProfileCreds'
@@ -14,8 +15,52 @@ class Profile extends Component {
       this.props.getProfileByHandle(this.props.match.params.handle)
     }
   }
+
+  componentWillReceiveProps (nextProps) {
+    if (nextProps.profile.profile === null && this.props.profile.loading) {
+      this.props.history.push('/not-found')
+    }
+  }
+
   render () {
-    return <div />
+    const { profile, loading } = this.props.profile
+    let profileContent
+
+    if (profile === null || loading) {
+      profileContent = <Spinner />
+    } else {
+      profileContent = (
+        <div>
+          <div className='row'>
+            <div className='col-md-6'>
+              <Link to='/profiles' className='btn btn-light mb-3 float-left'>
+                Back To Profiles
+              </Link>
+            </div>
+            <div className='col-md-6' />
+          </div>
+          <ProfileHeader profile={profile} />
+          <ProfileAbout profile={profile} />
+          <ProfileCreds
+            education={profile.education}
+            experience={profile.experience}
+          />
+          {profile.githubusername ? (
+            <ProfileGithub username={profile.githubusername} />
+          ) : null}
+        </div>
+      )
+    }
+
+    return (
+      <div className='profile'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-12'>{profileContent}</div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
@@ -23,6 +68,7 @@ Profile.propTypes = {
   getProfileByHandle: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired
 }
+
 const mapStateToProps = state => ({
   profile: state.profile
 })

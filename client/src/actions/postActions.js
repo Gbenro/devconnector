6e8465969fc015fdc0684768ffc430e5c1f2e1e6
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-27 18:47:42 -0600

    created postFeed component

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,5 +1,11 @@
 import axios from 'axios'
-import { ADD_POST, GET_ERRORS } from './types'
+import {
+  ADD_POST,
+  GET_ERRORS,
+  GET_POST,
+  POST_LOADING,
+  GET_POSTS
+} from './types'
 
 // Add post
 
@@ -18,4 +24,29 @@ export const addPost = postData => dispatch => {
         payload: err.response.data
       })
     )
+}
+// Get Post
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading())
+  axios
+    .get('api/posts')
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    )
+}
+
+// set loading state
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING
+  }
 }
